40187c5d8ace30b5f2f4146b96b7270f553bb7f1
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-03-05 20:28:39 +0600

    Прошлая домашка

---
+++ models /Track.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const TrackSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        album: {
+            type: mongoose.Schema.Types.ObjectID,
+            ref: 'Album',
+            required: true,
+        },
+        duration: String
+    },
+    {versionKey: false}
+ );
+
+const Track = mongoose.model('Track', TrackSchema);
+
+module.exports = Track;
